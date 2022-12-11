0d95a5bdbe5ab7bebaec1cf7a1eb059be4074092
Turbo Biscuit <55338035+trbodev@users.noreply.github.com>
2022-12-11 16:36:48 -0500

    refactor(docusaurus/theme/footer): refactor date parsing and footer replacement

--- src/theme/Footer/Copyright/index.js
+++ src/theme/Footer/Copyright/index.js
@@ -1,14 +1,13 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import React from 'react';
 export default function FooterCopyright({copyright}) {
-  let date = useDocusaurusContext().siteConfig.customFields.build.date;
-  let year = new Date(date).getFullYear();
+  let date = new Date(useDocusaurusContext().siteConfig.customFields.build.date);
   return (
     <div
       className="footer__copyright"
       // Developer provided the HTML, so assume it's safe.
       // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{__html: copyright.replace(year, new Date().getFullYear())}}
+      dangerouslySetInnerHTML={{__html: copyright.replace(date.getFullYear(), new Date().getFullYear())}}
     />
   );
 }
